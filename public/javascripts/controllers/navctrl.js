3d6edc2708b4e4f311cf0677158bd10b807ebf44
Mark Lee <markleechuyong@gmail.com>
2016-05-27 19:16:22 +0100

    logging out now returns to the main page by default

--- public/javascripts/controllers/navctrl.js
+++ public/javascripts/controllers/navctrl.js
@@ -5,8 +5,9 @@
 // Navigation controller
 app.controller('NavCtrl', [
     '$scope',
+    '$state',
     'auth',
-    function ($scope, auth) {
+    function ($scope, $state, auth) {
         $scope.isLoggedIn = auth.isLoggedIn;
         $scope.currentUser = auth.currentUser;
         $scope.logOut = auth.logOut;
