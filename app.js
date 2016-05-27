5ff317f504fc7f39d991c26b57449d375761427e
pk1914 <pavlos163@gmail.com>
2016-05-27 04:53:07 +0100

    Removed header from profile page.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
 // DB connection
 var mongoose = require('mongoose');
 require('./models/Posts');
