fc4f7de3acd62e8d43286bccbed4939aae61fbf1
Mark Lee <markleechuyong@gmail.com>
2016-05-30 00:31:43 +0100

    Added test graph for chronology data, fixed navbar to top

--- public/javascripts/controllers/chronoctrl.js
+++ public/javascripts/controllers/chronoctrl.js
@@ -92,5 +92,22 @@ app.controller('ChronoCtrl', [
 
         window.timeline = new TL.Timeline('chrono-timeline', timeline_json);
 
+        var trace1 = {
+            x: [0, 1, 2, 3, 4, 5],
+            y: [1.5, 1, 1.3, 0.7, 0.8, 0.9],
+            type: 'scatter'
+        };
+
+        var trace2 = {
+            x: [0, 1, 2, 3, 4, 5],
+            y: [1, 0.5, 0.7, -1.2, 0.3, 0.4],
+            type: 'bar'
+        };
+
+        data = [trace1, trace2];
+
+
+        Plotly.newPlot('chrono-display-1', data);
+
     }
 ]);
