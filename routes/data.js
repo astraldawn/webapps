e6f23e5af64f5f12077136d06a7f3256976abb55
Mark Lee <markleechuyong@gmail.com>
2016-05-29 23:11:40 +0100

    Set a limit on amount of data to return

--- routes/data.js
+++ routes/data.js
@@ -13,6 +13,8 @@ var PsychoData = mongoose.model('Psychodata');
 var DeviceData = mongoose.model('Devicedata');
 var FileData = mongoose.model('Filedata');
 
+var datalimit = 500;
+
 /* GET all chronological data */
 router.get('/chronodatas', function (req, res, next) {
     ChronoData.find(function (err, data) {
@@ -43,7 +45,7 @@ router.get('/allusers', function (req, res, next) {
 
 /* GET logon data from a specific username */
 router.param('data_logon', function (req, res, next, id) {
-    var query = LogonData.find({user_id: id});
+    var query = LogonData.find({user_id: id}).limit(datalimit);
 
     query.exec(function (err, data) {
         if (err) {
@@ -83,7 +85,7 @@ router.get('/userdata/psycho/:data_psycho', function (req, res) {
 
 /* GET device data from a specific username */
 router.param('data_device', function (req, res, next, id) {
-    var query = DeviceData.find({user_id: id});
+    var query = DeviceData.find({user_id: id}).limit(datalimit);
 
     query.exec(function (err, data) {
         if (err) {
@@ -104,7 +106,7 @@ router.get('/userdata/device/:data_device', function (req, res) {
 
 /* GET file data from a specific username */
 router.param('data_file', function (req, res, next, id) {
-    var query = FileData.find({user_id: id});
+    var query = FileData.find({user_id: id}).limit(datalimit);
 
     query.exec(function (err, data) {
         if (err) {
