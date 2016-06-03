1ab79700799bc7f22504b5807aac5681743a5437
Mark Lee Chu Yong <markleechuyong@gmail.com>
2016-06-03 23:12:47 +0100

    Created routes for date restricted query

--- routes/emaildata.js
+++ routes/emaildata.js
@@ -11,18 +11,28 @@ var async = require('async');
 var UserData = mongoose.model('Userdata');
 var EmailData = mongoose.model('Emaildata');
 
-function aggregateEmail(obj) {
+/*
+ Each query has a threshold * 100 % chance of running.
+
+ Params
+ - user_id
+ - start
+ - end
+ - min_weight
+ - threshold
+ */
+function aggregateEmail(user_id, min_weight, threshold, start, end) {
     return function (callback) {
         // var aggregate_query =
         // EmailData.aggregate().match({to: obj.user_id}).group({_id: '$from', count: {$sum: 1}});
         var process = Math.random();
-        if (process > 0.75) {
+        if (process < threshold) {
             var start = new Date("2010-01-01T00:00:00.0Z");
             var end = new Date("2010-01-02T00:00:00.0Z");
             EmailData.aggregate([
                     {
                         $match: {
-                            user_id: obj.user_id
+                            user_id: user_id
                             // date: {$gte: start, $lt: end}
                         }
                     },
@@ -31,8 +41,8 @@ function aggregateEmail(obj) {
                     }],
                 function (err, res) {
                     var output = res.map(function (item) {
-                        if (item.count > 100) {
-                            return {target: obj.user_id, source: item._id, value: item.count};
+                        if (item.count > min_weight) {
+                            return {target: user_id, source: item._id, value: item.count};
                         }
                     });
                     callback(null, output);
@@ -42,14 +52,13 @@ function aggregateEmail(obj) {
             callback(null, null)
         }
     };
-    // callback(null, obj.user_id);
 }
 
 /* GET email data from a specific department */
-router.param('department', function (req, res, next, id) {
+router.param('department', function (req, res, next, dept) {
     async.parallel({
             findUsers: function (cb) {
-                var query = UserData.find({department: id}, {_id: 0});
+                var query = UserData.find({department: dept}, {_id: 0});
                 query.select("user_id");
                 query.exec(cb);
             },
@@ -61,73 +70,158 @@ router.param('department', function (req, res, next, id) {
             departments: function (cb) {
                 var query = UserData.distinct("department");
                 query.exec(cb);
+            },
+            startDate: function (cb) {
+                cb();
+            },
+            endDate: function (cb) {
+                cb();
             }
-
         }, function (err, result) {
-            var aggregatefuncs = [];
-
-            var len = result.findUsers.length;
-            // len = 10;
-
-            for (var i = 0; i < len; i++) {
-                var aggregate_func = aggregateEmail(result.findUsers[i]);
-                aggregatefuncs.push(aggregate_func);
-            }
-
-            var emailMap = {};
-            for (i = 0; i < result.allUsers.length; i++) {
-                var email = result.allUsers[i].email;
-                emailMap[email] = {};
-                emailMap[email]["user_id"] = result.allUsers[i].user_id;
-                emailMap[email]["department"] = result.allUsers[i].department;
-            }
-
-            var departmentMap = {};
-            var external = result.departments.length;
-            for (i = 0; i < result.departments.length; i++) {
-                var department = result.departments[i];
-                departmentMap[department] = i;
-            }
-
-            async.parallel(
-                aggregatefuncs,
-                function (err, results) {
-                    var merged = [].concat.apply([], results);
-                    var query_dept = departmentMap[id];
-                    var output = [];
-                    for (i = 0; i < merged.length; i++) {
-                        if (merged[i] != undefined) {
-                            var cur = merged[i].source;
-                            var output_tmp = {};
-                            output_tmp["target"] = merged[i].target;
-                            output_tmp["source"] = cur;
-                            output_tmp["value"] = merged[i].value;
-                            if (emailMap[cur] !== undefined) {
-                                var cur_id = emailMap[cur]["user_id"];
-                                var cur_dept = emailMap[cur]["department"];
-                                output_tmp["source"] = cur_id;
-                                output_tmp["sd"] = departmentMap[cur_dept];
-                            } else {
-                                output_tmp["sd"] = external;
-                            }
-                            output_tmp["td"] = query_dept;
-                            output.push(output_tmp);
-                        }
-                    }
-                    res.json(output);
-                }
-            );
+            result.id = dept;
+            req.result = result;
+            return next();
         }
     );
-// };
-
-// process();
-})
-;
-
-router.get('/:department', function (req, res) {
-    res.json(req.data);
 });
+
+router.param('startdate', function (req, res, next, startDate) {
+    req.startDate = startDate;
+    return next();
+});
+
+router.param('enddate', function (req, res, next, endDate) {
+    req.endDate = endDate;
+    return next();
+});
+
+router.get('/:department/:startdate/:enddate', function (req, res) {
+    console.log("department - params");
+
+    var aggregatefuncs = [];
+
+    var result = req.result;
+    var id = req.id;
+    var startDate = req.startDate;
+    var endDate = req.endDate;
+    var len = result.findUsers.length;
+
+    var threshold = 0.4; // Each query has 40% chance of running
+    var min_weight = 100; // Minimum edge weight
+
+    for (var i = 0; i < len; i++) {
+        var c_user = result.findUsers[i].user_id;
+        var aggregate_func = aggregateEmail(c_user, min_weight, threshold, startDate, endDate);
+        aggregatefuncs.push(aggregate_func);
+    }
+
+    var emailMap = {};
+    for (i = 0; i < result.allUsers.length; i++) {
+        var email = result.allUsers[i].email;
+        emailMap[email] = {};
+        emailMap[email]["user_id"] = result.allUsers[i].user_id;
+        emailMap[email]["department"] = result.allUsers[i].department;
+    }
+
+    var departmentMap = {};
+    var external = result.departments.length;
+    for (i = 0; i < result.departments.length; i++) {
+        var department = result.departments[i];
+        departmentMap[department] = i;
+    }
+
+    async.parallel(
+        aggregatefuncs,
+        function (err, results) {
+            var merged = [].concat.apply([], results);
+            var query_dept = departmentMap[id];
+            var output = [];
+            for (i = 0; i < merged.length; i++) {
+                if (merged[i] != undefined) {
+                    var cur = merged[i].source;
+                    var output_tmp = {};
+                    output_tmp["target"] = merged[i].target;
+                    output_tmp["source"] = cur;
+                    output_tmp["value"] = merged[i].value;
+                    if (emailMap[cur] !== undefined) {
+                        var cur_id = emailMap[cur]["user_id"];
+                        var cur_dept = emailMap[cur]["department"];
+                        output_tmp["source"] = cur_id;
+                        output_tmp["sd"] = departmentMap[cur_dept];
+                    } else {
+                        output_tmp["sd"] = external;
+                    }
+                    output_tmp["td"] = query_dept;
+                    output.push(output_tmp);
+                }
+            }
+            res.json(output);
+        }
+    );
+});
+
+// router.get('/:department', function (req, res) {
+//     console.log("department - non param");
+//
+//     var aggregatefuncs = [];
+//
+//     var result = req.result;
+//     var id = req.id;
+//     var len = result.findUsers.length;
+//
+//     var threshold = 0.4; // Each query has 40% chance of running
+//     var min_weight = 100; // Minimum edge weight
+//
+//     for (var i = 0; i < len; i++) {
+//         var c_user = result.findUsers[i].user_id;
+//         var aggregate_func = aggregateEmail(c_user, min_weight, threshold, null, null);
+//         aggregatefuncs.push(aggregate_func);
+//     }
+//
+//     var emailMap = {};
+//     for (i = 0; i < result.allUsers.length; i++) {
+//         var email = result.allUsers[i].email;
+//         emailMap[email] = {};
+//         emailMap[email]["user_id"] = result.allUsers[i].user_id;
+//         emailMap[email]["department"] = result.allUsers[i].department;
+//     }
+//
+//     var departmentMap = {};
+//     var external = result.departments.length;
+//     for (i = 0; i < result.departments.length; i++) {
+//         var department = result.departments[i];
+//         departmentMap[department] = i;
+//     }
+//
+//     async.parallel(
+//         aggregatefuncs,
+//         function (err, results) {
+//             var merged = [].concat.apply([], results);
+//             var query_dept = departmentMap[id];
+//             var output = [];
+//             for (i = 0; i < merged.length; i++) {
+//                 if (merged[i] != undefined) {
+//                     var cur = merged[i].source;
+//                     var output_tmp = {};
+//                     output_tmp["target"] = merged[i].target;
+//                     output_tmp["source"] = cur;
+//                     output_tmp["value"] = merged[i].value;
+//                     if (emailMap[cur] !== undefined) {
+//                         var cur_id = emailMap[cur]["user_id"];
+//                         var cur_dept = emailMap[cur]["department"];
+//                         output_tmp["source"] = cur_id;
+//                         output_tmp["sd"] = departmentMap[cur_dept];
+//                     } else {
+//                         output_tmp["sd"] = external;
+//                     }
+//                     output_tmp["td"] = query_dept;
+//                     output.push(output_tmp);
+//                 }
+//             }
+//             res.json(output);
+//         }
+//     );
+// });
 
 
 module.exports = router;
