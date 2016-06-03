e86d718232dd42170edc323dcce29574f437f82a
Lee De Sheng Royson <de.lee14@imperial.ac.uk>
2016-06-03 01:28:31 +0100

    Trivial changes.

--- public/javascripts/controllers/graphnodectrl.js
+++ public/javascripts/controllers/graphnodectrl.js
@@ -74,7 +74,6 @@ function GraphNodeCtrl($scope, $http) {
 
             var zoom = d3.behavior.zoom()
             .scaleExtent([0.1, 10])
-            .scale(0.1)
             .on("zoom", zoomed);
 
             var svg = d3.select(graphID).append("svg")
