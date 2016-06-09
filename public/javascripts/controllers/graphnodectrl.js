b1ae2e4f2a8969515f0a23fd32aec84433ac331b
Lee De Sheng Royson <de.lee14@imperial.ac.uk>
2016-06-09 18:08:19 +0100

    legend.

--- public/javascripts/controllers/graphnodectrl.js
+++ public/javascripts/controllers/graphnodectrl.js
@@ -166,28 +166,6 @@ function GraphNodeCtrl($scope, $http) {
                 .attr('height', height)
                 .attr('fill', 'none');
 
-            var legend = svg.selectAll(".legend")
-                .data(color.domain())
-                .enter().append("g")
-                .attr("class", "legend")
-                .attr("transform", function(d, i) { 
-                    return "translate(0," + i * 20 + ")"; });
-
-            legend.append("rect")
-                .attr("x", width - 18)
-                .attr("width", 18)
-                .attr("height", 18)
-                .style("fill", color);
-
-            legend.append("text")
-                .attr("x", width - 24)
-                .attr("y", 9)
-                .attr("dy", ".35em")
-                .style("text-anchor", "end")
-                .text(function(d) {
-                    var desc = $scope.availableDept[d-1] || "External";
-                    return desc; });
-
             // Arrows
             svg.append("svg:defs").selectAll("marker")
                 .data(["end"])
@@ -232,6 +210,29 @@ function GraphNodeCtrl($scope, $http) {
                 .text(function (d) {
                     return d.name;
                 });
+
+            
+            var legend = svg.selectAll(".legend")
+                .data(color.domain())
+                .enter().append("g")
+                .attr("class", "legend")
+                .attr("transform", function(d, i) { 
+                    return "translate(0," + i * 20 + ")"; });
+
+            legend.append("rect")
+                .attr("x", width - 18)
+                .attr("width", 18)
+                .attr("height", 18)
+                .style("fill", color);
+
+            legend.append("text")
+                .attr("x", width - 24)
+                .attr("y", 9)
+                .attr("dy", ".35em")
+                .style("text-anchor", "end")
+                .text(function(d) {
+                    var desc = $scope.availableDept[d-1] || "External";
+                    return desc; });
 
             // Curve lines
             function tick() {
