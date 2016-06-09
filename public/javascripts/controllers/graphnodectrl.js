8512f15829239cbbb7e13c25b39bbb88f02cf86f
Mark Lee Chu Yong <markleechuyong@gmail.com>
2016-06-09 21:57:47 +0100

    more ui tweaks

--- public/javascripts/controllers/graphnodectrl.js
+++ public/javascripts/controllers/graphnodectrl.js
@@ -131,7 +131,7 @@ function GraphNodeCtrl($scope, $http) {
             clearData(graphID);
 
             var width = d3.select(graphID).style("width").split("px").shift();
-            var height = width * 0.75;
+            var height = width * 0.60;
 
             var color = d3.scale.category20();
 
